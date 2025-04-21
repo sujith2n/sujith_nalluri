e7b6a8ff4aa5557e08eacc3020afb1dc939aac1b
sujith2n <sujith2n@gmail.com>
2025-04-21 16:22:00 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,19 @@
+"""Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen."""
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+print("Addition : ",add)
+print("Subtraction : ",sub)
+print("multiplication : ",multi)
+print("Division : ",divi)

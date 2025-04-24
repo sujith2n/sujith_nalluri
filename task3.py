8c3707ba24691bd0cfa6918843e605ffaff3fc9d
sujith2n <sujith2n@gmail.com>
2025-04-24 10:05:58 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,8 @@
+# Step 1: Take input from the user
+number = int(input("Enter an integer: "))
+
+# Step 2: Use if-else to check if the number is even or odd
+if number % 2 == 0:
+    print(f"The number {number} is Even.")
+else:
+    print(f"The number {number} is Odd.")
---
+++ task4.py
@@ -0,0 +1,7 @@
+#initialize the sum
+total_sum = 0
+# Iterate over numbers from 1 to 50
+for num in range(1,51):
+    total_sum += num
+# Display the final sum
+print("the sum of number from 1 to 50 is : ",total_sum)
